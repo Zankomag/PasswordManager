6e1deb4935105d6cf43776fc07cd49e418043fd0
Zankomag <zankomag@gmail.com>
2019-06-12 15:16:07 +0300

    Add encryption and pwdGenerator

--- src/UPwdBot/BotHandler.cs
+++ src/UPwdBot/BotHandler.cs
@@ -24,22 +24,33 @@ namespace UPwdBot {
 		}
 
 		public async Task HandleMessage(Message message) {
-			string accountName = message.Text.Length <= 50 ? message.Text : message.Text.Remove(49);
-			using (var cmd = new MySqlCommand(
-				"SELECT accountName, link, login " +
-				" FROM passwordManager.accounts " +
-				"where userId = " + message.From.Id +
-				" and accountName like '%" + accountName + "%'",
-				Bot.Instance.Connection)) {
 
-				using (var reader = cmd.ExecuteReader()) {
-					while (reader.Read()) {
-						await bot.SendTextMessageAsync(message.From.Id, reader.GetString(0) + Environment.NewLine
-							+ reader.GetString(1)
-							+ Environment.NewLine + reader.GetString(2), disableWebPagePreview: true);
-					}
-				}
+			if(message.Text[0] == '/' && message.Text.Length > 1) {
+				string decString = Uten.Encryption.Encryption.Decrypt(message.Text.Substring(1));
+				await bot.SendTextMessageAsync(message.From.Id, decString);
+				return;
 			}
+
+			string encryptedMessage = Uten.Encryption.Encryption.Encrypt(message.Text);
+			await bot.SendTextMessageAsync(message.From.Id, encryptedMessage);
+
+
+			//string accountName = message.Text.Length <= 50 ? message.Text : message.Text.Remove(49);
+			//using (var cmd = new MySqlCommand(
+			//	"SELECT accountName, link, login " +
+			//	" FROM passwordManager.accounts " +
+			//	"where userId = " + message.From.Id +
+			//	" and accountName like '%" + accountName + "%'",
+			//	Bot.Instance.Connection)) {
+
+			//	using (var reader = cmd.ExecuteReader()) {
+			//		while (reader.Read()) {
+			//			await bot.SendTextMessageAsync(message.From.Id, reader.GetString(0) + Environment.NewLine
+			//				+ reader.GetString(1)
+			//				+ Environment.NewLine + reader.GetString(2), disableWebPagePreview: true);
+			//		}
+			//	}
+			//}
 		}
 	}
 }
---
+++ src/UPwdBot/Password.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Uten.Passwords {
+	public class Password {
+		public static string Generate(uint length = 32,
+			bool containsLowerChars = true,
+			bool containsUpperChars = true,
+			bool containsDigits = true,
+			bool containsSpecialChars = true, 
+			bool firstCharIsLetter = true) {
+
+		
+			if (firstCharIsLetter && !containsLowerChars ||
+				firstCharIsLetter && !containsUpperChars)
+				throw new ArgumentNullException("firstCharIsLetter", "Cannot set letter as first character, because it does not exist.");
+
+			string charList = "";
+			if (containsLowerChars)
+				charList += "abcdefghijklmnopqrstuvwxyz";
+			if (containsUpperChars)
+				charList += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+			if (containsDigits)
+				charList += "0123456789";
+			if (containsSpecialChars) {
+				charList += @"`~!@#$%^&*()_-+={}[]\|:;<>,.?/";
+			}
+
+			if (charList.Length == 0) throw new ArgumentNullException("No option selected");
+
+			Random random = new Random();
+			string pwd = "";
+
+			if (firstCharIsLetter) {
+				pwd += charList[random.Next(0, 53)];
+				length--;
+			}
+
+			for (int i = 0; i < length; i++)
+				pwd += charList[random.Next(0, charList.Length)];
+
+			return pwd;
+		}
+	}
+}
