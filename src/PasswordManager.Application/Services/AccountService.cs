a2ff4a2bffddc61778013b0a8f54d5b47e9b6dfb
Adam Evans <Zankomag@gmail.com>
2020-12-08 00:48:14 +0200

    Add AccountService

---
+++ src/PasswordManager.Application/Services/AccountService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PasswordManager.Application.Services.Abstractions;
+
+namespace PasswordManager.Application.Services {
+	class AccountService : IAccountService {
+
+	}
+}
