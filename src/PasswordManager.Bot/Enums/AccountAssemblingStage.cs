d6bdcb1b4a431708b3488a881c524cc3d4ad2538
Adam Evans <Zankomag@gmail.com>
2020-12-08 22:11:44 +0200

    Create AccountAssemblingStage.cs

---
+++ src/PasswordManager.Bot/Enums/AccountAssemblingStage.cs
@@ -0,0 +1,12 @@
+﻿
+namespace PasswordManager.Bot.Enums {
+	public enum AccountAssemblingStage {
+		AddAccountName = 0,
+		AddLink,
+		AddNote,
+		AddLogin,
+		AddPassword,
+		EncryptPassword,
+		Release
+	}
+}
