05d2153ebba4561d2b68728bd334d038ca8ee43c
Adam Evans <Zankomag@gmail.com>
2020-12-08 08:13:13 +0200

    Code refactoring (BotHandler)

--- src/PasswordManager.Bot/BotHandler.cs
+++ src/PasswordManager.Bot/BotHandler.cs
@@ -1,42 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using MultiUserLocalization;
+using PasswordManager.Application.Services.Abstractions;
+using PasswordManager.Bot.Abstractions;
+using PasswordManager.Bot.Commands;
+using PasswordManager.Bot.Commands.Abstractions;
+using PasswordManager.Bot.Models;
+using PasswordManager.Bot.Types.Enums;
+using PasswordManager.Core.Entities;
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
-using PasswordManager.Bot.Commands;
-using PasswordManager.Bot.Types.Enums;
-using MultiUserLocalization;
-using PasswordManager.Bot.Commands.Abstractions;
-using PasswordManager.Core.Entities;
-using PasswordManager.Bot.Abstractions;
-using PasswordManager.Core.Repositories;
-using System.Linq;
-using PasswordManager.Bot.Models;
-using PasswordManager.Application.Services.Abstractions;
 using User = PasswordManager.Core.Entities.User;
+using PasswordManager.Bot.Extensions;
 
 namespace PasswordManager.Bot {
 	public class BotHandler : IBotHandler {
 		private readonly IBotService botService;
 		private readonly IUserService userService;
-		
+		private readonly ICommandFactory commandFactory;
+
 
 		private static readonly SelectLanguageCommand selectLanguageCommand = new SelectLanguageCommand();
 
-		public BotHandler(IBotService botService, IUserService userService) {
+		public BotHandler(IBotService botService, IUserService userService, ICommandFactory commandFactory) {
 			this.botService = botService;
 			this.userService = userService;
+			this.commandFactory = commandFactory;
 		}
 
 		public async void HandleUpdate(Update update) {
 			switch (update.Type) {
 				case UpdateType.Message:
-				if (update.Message.Type == MessageType.Text)
-					await HandleMessage(update.Message);
-				return;
+					if (update.Message.Type == MessageType.Text)
+						await HandleMessage(update.Message);
+					return;
 				case UpdateType.CallbackQuery:
 					await HandleCallbackQuery(update.CallbackQuery);
-				return;
+					return;
 			}
 		}
 
@@ -62,8 +63,7 @@ namespace PasswordManager.Bot {
 							});
 							return;
 						}
-					} 
-					else {
+					} else {
 						return;
 					}
 				}
@@ -72,34 +72,35 @@ namespace PasswordManager.Bot {
 					Lang = userEntity.Lang,
 					Action = userEntity.Action
 				};
-				
 
-				string commandText = null;
 
-				//Command that starts with '/' may contain args and must be separated
-				if (message.Text.StartsWith('/')) {
-					string commandString = message.Text.ToLower();
-					int cIndex = commandString.IndexOfAny(new char[] { ' ', '\n' });
-					commandText = cIndex != -1 ? commandString.Substring(0, cIndex) : commandString;
-				}
+				string commandText = message.Text.GetTextCommand();
 
-				if (commandText != null && messageCommands.TryGetValue(commandText, out IMessageCommand command)) {
-					await command.ExecuteAsync(message, user);
-				}
-				else {
-					try {
-						await actionCommands[user.Action].ExecuteAsync(message, user);
+				if (commandText != null) {
+					var messageCommand = commandFactory.GetMessageCommand(commandText);
+					if (messageCommand != null) {
+						await messageCommand.ExecuteAsync(message, user);
+					} else {
+						try {
+							await botService.Client.SendTextMessageAsync(message.From.Id,
+									text: Localization.GetMessage("UnknownCommand", user.Lang));
+						} catch { }
 					}
-					catch (KeyNotFoundException exception) {
-						userEntity.Action = UserAction.Search;
-						user.Action = UserAction.Search;
-						await userService.UpdateActionAsync(userEntity);
-						await actionCommands[UserAction.Search].ExecuteAsync(message, user);
-						//TODO: Log exception
+				} else {
+					var actionCommand = commandFactory.GetActionCommand(user.Action);
+					if (actionCommand != null) {
+						await actionCommand.ExecuteAsync(message, user);
+					} else {
+						//If ActionCommand is unknown - hanlde it as Search
+						if (user.Action != UserAction.Search) {
+							userEntity.Action = UserAction.Search;
+							user.Action = UserAction.Search;
+							await userService.UpdateActionAsync(userEntity);
+						}
+						await commandFactory.GetActionCommand(UserAction.Search).ExecuteAsync(message, user);
 					}
 				}
-			}
-			catch (Exception ex) {
+			} catch (Exception ex) {
 				botService.SendMessageToAllAdmins(ex.ToString());
 				//TODO: Log Exception
 				throw;
@@ -108,9 +109,8 @@ namespace PasswordManager.Bot {
 
 
 		private async Task HandleCallbackQuery(CallbackQuery callbackQuery) {
-			BotUser logUser = null;
+			BotUser user = null;
 			try {
-				BotUser user = null;
 				User userEntity = await userService.GetLangAsync(callbackQuery.From.Id);
 				if (userEntity == null) {
 					//Add new user to db when he selected language for the first time
@@ -118,12 +118,10 @@ namespace PasswordManager.Bot {
 					//ONLY If unauthorized user is admin - bot treats them as new user
 					//Not-admin users must be added to bot by Admin manually
 					//If you want to allow free registration in your bot for any user - disable this admin check
-					if (botService.Admins.Contains(callbackQuery.From.Id))
-					{
+					if (botService.Admins.Contains(callbackQuery.From.Id)) {
 						//TODO:
 						//Separate adding new account logic to othee method
-						if (callbackQuery.Data[0] == (char)CallbackCommandCode.SelectLanguage)
-						{
+						if (callbackQuery.Data[0] == (char)CallbackCommandCode.SelectLanguage) {
 							userEntity = await userService
 								.AddUserAsync(callbackQuery.From.Id, Localization.DefaultLanguageCode);
 							user = new BotUser {
@@ -137,31 +135,36 @@ namespace PasswordManager.Bot {
 					}
 					return;
 				}
-				logUser = user;
-				
 
-				ICallbackQueryCommand command;
-				if (callBackCommands.TryGetValue((CallbackCommandCode)callbackQuery.Data[0], out command)) {
-					await command.ExecuteAsync(callbackQuery, user);
+				//TODO: Rename
+				CallbackCommandCode callbackCommandCode;
+				try {
+					callbackCommandCode = (CallbackCommandCode)callbackQuery.Data[0];
+				} catch(Exception exeption) {
+					//TODO: Log Exception
+					throw;
 				}
-				else {
+
+				var callbackQueryCommand = commandFactory.GetCallBackQueryCommand(callbackCommandCode);
+				if(callbackQueryCommand != null) {
+					await callbackQueryCommand.ExecuteAsync(callbackQuery, user);
+				} else {
 					try {
-						await botService.Client.AnswerCallbackQueryAsync(callbackQuery.Id, text: "Unknown command", showAlert: true);
+						await botService.Client.AnswerCallbackQueryAsync(callbackQuery.Id,
+							text: Localization.GetMessage("UnknownCommand", user.Lang),
+							showAlert: true);
 					} catch { }
 				}
-				
-			}
-			catch (Telegram.Bot.Exceptions.InvalidParameterException exeption) {
+
+			} catch (Telegram.Bot.Exceptions.InvalidParameterException exeption) {
 				//TODO: Log Exception
-			}
-			catch (Exception exeption) {
+			} catch (Exception exeption) {
 				string message = "EXCEPTION: " + exeption.ToString();
-				if(logUser != null)
-					message += "\n\nUSER: [user](tg://user?id=" + logUser.Id.ToString() + ")";
+				if (user != null)
+					message += "\n\nUSER: [user](tg://user?id=" + user.Id.ToString() + ")";
 				botService.SendMessageToAllAdmins(message);
 				//TODO: Log exception
 			}
-
 		}
 
 	}
