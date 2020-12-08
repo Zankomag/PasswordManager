ae70a2720a716146022f9b76f550ccc4226c8b7e
Adam Evans <Zankomag@gmail.com>
2020-12-08 09:10:53 +0200

    Add Base BotCommand

---
+++ src/PasswordManager.Bot/Commands/Abstractions/BotCommand.cs
@@ -0,0 +1,12 @@
+﻿using PasswordManager.Bot.Abstractions;
+
+namespace PasswordManager.Bot.Commands.Abstractions {
+	public abstract class BotCommand : IBotCommand {
+		protected readonly IBotService botService;
+
+		public BotCommand(IBotService botService) {
+			this.botService = botService;
+		}
+
+	}
+}
