d6e6a93da37203669d4acf7b3d7612eb46b69b6c
Adam Evans <Zankomag@gmail.com>
2020-12-08 20:23:02 +0200

    Add Valodation to AssembleAccountModel

--- src/PasswordManager.Bot/Models/AssembleAccountModel.cs
+++ src/PasswordManager.Bot/Models/AssembleAccountModel.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using PasswordManager.Core.Entities;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -6,14 +6,71 @@ using System.Linq;
 namespace PasswordManager.Bot.Models {
 	public class AssembleAccountModel {
 		public long Id { get; set; }
+
 		public int UserId { get; set; }
-		[StringLength(60)] public string AccountName { get; set; }
-		[StringLength(70)] public string Link { get; set; }
-		[StringLength(40)] public string Login { get; set; }
-		[StringLength(8096)] public string Password { get; set; }
-		[StringLength(1024)] public string Note { get; set; }
+
+		private string accountName;
+		public string AccountName { get => accountName;
+			set {
+				var account = new Account { AccountName = value };
+				Validate(account.AccountName, account, nameof(account.AccountName));
+				accountName = value;
+			} }
+
+		private string link;
+		public string Link {
+			get => link;
+			set {
+				var account = new Account { Link = value };
+				Validate(account.Link, account, nameof(account.Link));
+				link = value;
+			}
+		}
+
+		private string login;
+		public string Login {
+			get => login;
+			set {
+				var account = new Account { Login = value };
+				Validate(account.Login, account, nameof(account.Login));
+				login = value;
+			}
+		}
+
+		private string password;
+		public string Password {
+			get => password;
+			set {
+				var account = new Account { Password = value };
+				Validate(account.Password, account, nameof(account.Password));
+				password = value;
+			}
+		}
+
+		private string note;
+		public string Note {
+			get => note;
+			set {
+				var account = new Account { Note = value };
+				Validate(account.Note, account, nameof(account.Note));
+				note = value;
+			}
+		}
+
 		public bool Encrypted { get; set; }
 
 		public bool SkipLink { get; set; }
+
+		//TODO:
+		//DEbug this
+		//
+		//Value must be property of instance
+		//memberName must be namof(instance.Property)
+		private void Validate(object value, Account instance, string memberName) {
+			ICollection<ValidationResult> validationResults = null;
+			if (!Validator.TryValidateProperty(value, new ValidationContext(instance) {MemberName = memberName }, validationResults)) {
+				throw new ValidationException(validationResults?.FirstOrDefault().ErrorMessage);
+			}
+		}
 	}
 }
