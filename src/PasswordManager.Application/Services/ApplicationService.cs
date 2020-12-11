a60b28ae8afdf1d038119ee07f0ad822fdac344b
Adam Evans <Zankomag@gmail.com>
2020-12-11 23:56:36 +0200

    Add ApplicationService

---
+++ src/PasswordManager.Application/Services/ApplicationService.cs
@@ -0,0 +1,13 @@
+﻿using PasswordManager.Application.Services.Abstractions;
+using Microsoft.Extensions.Options;
+
+namespace PasswordManager.Application.Services {
+	public class ApplicationService : IApplicationService {
+		public int[] Admins { get; }
+
+		public ApplicationService(IOptions<ApplicationSettings> appSettingsConfig) {
+			ApplicationSettings appSettings = appSettingsConfig.Value;
+			Admins = appSettings.AdminIds;
+		}
+	}
+}
