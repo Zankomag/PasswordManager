4f02e633dcd86006f9e92c5b8b7733173cde28cc
Adam Evans <Zankomag@gmail.com>
2020-12-12 02:58:42 +0200

    Update StringExtensions

--- src/PasswordManager.Bot/Extensions/StringExtensions.cs
+++ src/PasswordManager.Bot/Extensions/StringExtensions.cs
@@ -53,20 +53,28 @@ namespace PasswordManager.Bot.Extensions {
 		public static string ToStringCode(this CallbackQueryCommandCode callbackCommandCode) 
 			=> ((char)callbackCommandCode).ToString();
 
+		///<summary></summary>
 		/// <param name="setUpPasswordGeneratorCommandCode"></param>
 		/// <returns><see cref="CallbackQueryCommandCode.SetUpPasswordGenerator"/> + setUpPasswordGeneratorCommandCode</returns>
 		public static string ToStringCode(this SetUpPasswordGeneratorCommandCode setUpPasswordGeneratorCommandCode)
-			=> GetStringCode(CallbackQueryCommandCode.SetUpPasswordGenerator, (char)setUpPasswordGeneratorCommandCode);	
+			=> GetStringCode(CallbackQueryCommandCode.SetUpPasswordGenerator, (char)setUpPasswordGeneratorCommandCode);
 
+		///<summary></summary>
 		/// <param name="addAccountCommandCode"></param>
 		/// <returns><see cref="CallbackQueryCommandCode.AddAccount"/> + addAccountCommandCode</returns>
 		public static string ToStringCode(this AddAccountCommandCode addAccountCommandCode)
 			=> GetStringCode(CallbackQueryCommandCode.AddAccount, (char)addAccountCommandCode);
 
+		///<summary></summary>
 		/// <param name="selectLanguageCommandCode"></param>
 		/// <returns><see cref="CallbackQueryCommandCode.SelectLanguage"/> + selectLanguageCommandCode</returns>
 		public static string ToStringCode(this SelectLanguageCommandCode selectLanguageCommandCode) 
 			=> GetStringCode(CallbackQueryCommandCode.SelectLanguage, (char)selectLanguageCommandCode);
+		///<summary></summary>
+		/// <param name="updateAccountCommandCode"></param>
+		/// <returns><see cref="CallbackQueryCommandCode.UpdateAccount"/> + updateAccountCommandCode</returns>
+		public static string ToStringCode(this UpdateAccountCommandCode updateAccountCommandCode)
+			=> GetStringCode(CallbackQueryCommandCode.UpdateAccount, (char)updateAccountCommandCode);
 
 		/// <summary></summary>
 		/// <param name="additionalCommand">Command that need to be appended to <paramref name="callbackQueryCommandCode"/></param>
