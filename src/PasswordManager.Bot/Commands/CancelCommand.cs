e72ee29c6bf7ef2c5f22384ac71ae822f08d4972
Adam Evans <Zankomag@gmail.com>
2020-12-14 05:28:09 +0200

    Bug fix (CancelCommand)

--- src/PasswordManager.Bot/Commands/CancelCommand.cs
+++ src/PasswordManager.Bot/Commands/CancelCommand.cs
@@ -11,20 +11,56 @@ namespace PasswordManager.Bot.Commands {
 	public class CancelCommand : Abstractions.BotCommand, IMessageCommand {
 		private readonly IUserService userService;
 		private readonly IAccountAssemblingService accountAssemblingService;
+		private readonly IAccountUpdatingService accountUpdatingService;
+		private readonly IPasswordDecryptionService passwordDecryptionService;
+		private readonly IPasswordEncryptionService passwordEncryptionService;
 
 		public CancelCommand(IBot bot,
 			IUserService userService,
-			IAccountAssemblingService accountAssemblingService)
+			IAccountAssemblingService accountAssemblingService,
+			IAccountUpdatingService accountUpdatingService,
+			IPasswordDecryptionService passwordDecryptionService,
+			IPasswordEncryptionService passwordEncryptionService)
 			: base(bot) {
 
 			this.userService = userService;
 			this.accountAssemblingService = accountAssemblingService;
+			this.accountUpdatingService = accountUpdatingService;
+			this.passwordDecryptionService = passwordDecryptionService;
+			this.passwordEncryptionService = passwordEncryptionService;
 		}
 
+		//TODO:
+		//instead of userService.UpdateUserAction we need to call
+		//PasswordManager.Bot.Services.UserActionService.ResetUserAction(user.Id) so
+		//if there is attempt to override action this service checks whether there is unfinished action
+		//and clears its data
 		async Task IMessageCommand.ExecuteAsync(Message message, BotUser user) {
 			if (user.Action != UserAction.Search) {
-				if(user.Action == UserAction.AssembleAccount)
-					accountAssemblingService.Cancel(user.Id);
+				switch (user.Action) {
+					case UserAction.AssembleAccount:
+						accountAssemblingService.Cancel(user.Id);
+						break;
+					case UserAction.UpdateAccount:
+						accountUpdatingService.FinishUpdatingRequest(user.Id);
+						break;
+					case UserAction.EnterDecryptionKey:
+						passwordDecryptionService.FinishDecryptionRequest(user.Id);
+						break;
+					case UserAction.EncryptPassword:
+						passwordEncryptionService.FinishEncryptionRequest(user.Id);
+						break;
+						//TODO:
+						//Clear UserSettingsUpdatingservice request here
+						//
+						//TODO:
+						//If UserAction.SetUpPasswordGeneratorLength expands to having setuppasswordgenerator service
+						//finish remaining requests here
+						//
+						//UserAction.SetUpPasswordGeneratorLength does not need to be handeled here, 
+						//as updating action to search alredy cancels action
+				}
+
 				await userService.UpdateActionAsync(user.Id, UserAction.Search);
 				await bot.Client.SendTextMessageAsync(message.From.Id,
 					Localization.GetMessage("Cancel", user.Lang));
