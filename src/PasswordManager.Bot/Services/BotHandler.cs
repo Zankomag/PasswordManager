0b4aaf100fe5e9479c415b0de5a8d0d8fad92659
Adam Evans <Zankomag@gmail.com>
2021-07-26 00:42:11 +0300

    refactor

--- src/PasswordManager.Bot/Services/BotHandler.cs
+++ src/PasswordManager.Bot/Services/BotHandler.cs
@@ -1,19 +1,19 @@
-﻿using MultiUserLocalization;
+﻿using System;
+using System.Threading.Tasks;
+using MultiUserLocalization;
 using PasswordManager.Application.Services.Abstractions;
-using PasswordManager.Bot.Services.Abstractions;
 using PasswordManager.Bot.Commands.Abstractions;
 using PasswordManager.Bot.Commands.Enums;
 using PasswordManager.Bot.Extensions;
 using PasswordManager.Bot.Models;
-using PasswordManager.Core.Entities;
-using System;
-using System.Threading.Tasks;
+using PasswordManager.Bot.Services.Abstractions;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 
 namespace PasswordManager.Bot.Services {
+
 	/// <summary>
-	/// Handles Telegram Bot Updates
+	///     Handles Telegram Bot Updates
 	/// </summary>
 	public class BotHandler : IBotHandler {
 		private readonly IBot bot;
@@ -23,44 +23,48 @@ namespace PasswordManager.Bot.Services {
 
 		//TODO:
 		//Make BaseBotHandler for framework
-		public BotHandler(IBot bot, IUserService userService, ICommandFactory commandFactory, IBotUserService botUserService) {
+		public BotHandler(IBot bot, IUserService userService, ICommandFactory commandFactory,
+			IBotUserService botUserService) {
 			this.bot = bot;
 			this.userService = userService;
 			this.commandFactory = commandFactory;
 			this.botUserService = botUserService;
 		}
 
-		public async virtual Task HandleUpdateAsync(Update update) { 
+		public virtual async Task HandleUpdateAsync(Update update) {
 			Func<BotUser, Task> handleUpdateFunc = update.Type switch {
-				UpdateType.Message => (Func<BotUser, Task>)(async (BotUser botUser) => {
-					if (update.Message.Type == MessageType.Text)
+				UpdateType.Message => async botUser => {
+					if(update.Message.Type == MessageType.Text)
 						await HandleMessageAsync(update.Message, botUser);
-				}),
-				UpdateType.CallbackQuery => (Func<BotUser, Task>)(async (BotUser botUser)
-					=> await HandleCallbackQueryAsync(update.CallbackQuery, botUser)),
+				},
+				UpdateType.CallbackQuery => async botUser
+					=> await HandleCallbackQueryAsync(update.CallbackQuery, botUser),
 				_ => null
 			};
+
 			//We do not call the database if there is no reason to retrieve the user
-			if (handleUpdateFunc != null) {
+			if(handleUpdateFunc != null) {
 				BotUser botUser = null;
-				if ((botUser = await GetUser(update)) != null)
+				if((botUser = await GetUser(update)) != null)
 					await handleUpdateFunc(botUser);
 			}
 		}
 
-		protected async virtual Task<BotUser> GetUser(Update update)
+		protected virtual async Task<BotUser> GetUser(Update update)
 			=> await botUserService.GetUser(update);
-		
 
-		protected async virtual Task HandleMessageAsync(Message message, BotUser user) {
-			if (message == null) {
+
+		protected virtual async Task HandleMessageAsync(Message message, BotUser user) {
+			if(message == null) {
 				ArgumentNullException exception = new ArgumentNullException(nameof(message));
+
 				//TODO:
 				//Log exception
 				throw exception;
-			} 
-			if (user == null) {
+			}
+			if(user == null) {
 				ArgumentNullException exception = new ArgumentNullException(nameof(user));
+
 				//TODO:
 				//Log exception
 				throw exception;
@@ -68,9 +72,9 @@ namespace PasswordManager.Bot.Services {
 
 			string commandText = message.Text.GetTextCommand();
 
-			if (commandText != null) {
+			if(commandText != null) {
 				var messageCommand = commandFactory.GetMessageCommand(commandText);
-				if (messageCommand != null) {
+				if(messageCommand != null) {
 					await messageCommand.ExecuteAsync(message, user);
 				} else {
 					try {
@@ -86,11 +90,11 @@ namespace PasswordManager.Bot.Services {
 					}
 				}
 				var actionCommand = commandFactory.GetActionCommand(user.Action);
-				if (actionCommand != null) {
+				if(actionCommand != null) {
 					await actionCommand.ExecuteAsync(message, user);
 				} else {
 					//If ActionCommand is unknown - hanlde it as Search
-					if (user.Action != default) {
+					if(user.Action != default) {
 						user.Action = default;
 						await userService.UpdateActionAsync(user.Id, default);
 					}
@@ -101,30 +105,27 @@ namespace PasswordManager.Bot.Services {
 		}
 
 
-		protected async virtual Task HandleCallbackQueryAsync(CallbackQuery callbackQuery, BotUser user) {
-			if (callbackQuery == null) {
+		protected virtual async Task HandleCallbackQueryAsync(CallbackQuery callbackQuery, BotUser user) {
+			if(callbackQuery == null) {
 				ArgumentNullException exception = new ArgumentNullException(nameof(callbackQuery));
+
 				//TODO:
 				//Log exception
 				throw exception;
 			}
-			if (user == null) {
+			if(user == null) {
 				ArgumentNullException exception = new ArgumentNullException(nameof(user));
+
 				//TODO:
 				//Log exception
 				throw exception;
 			}
 
 			CallbackQueryCommandCode callbackCommandCode;
-			try {
-				callbackCommandCode = (CallbackQueryCommandCode)callbackQuery.Data[0];
-			} catch (Exception exception) {
-				//TODO: Log Exception
-				throw;
-			}
+			callbackCommandCode = (CallbackQueryCommandCode)callbackQuery.Data[0];
 
 			var callbackQueryCommand = commandFactory.GetCallBackQueryCommand(callbackCommandCode);
-			if (callbackQueryCommand != null) {
+			if(callbackQueryCommand != null) {
 				await callbackQueryCommand.ExecuteAsync(callbackQuery, user);
 			} else {
 				try {
@@ -133,18 +134,15 @@ namespace PasswordManager.Bot.Services {
 			}
 		}
 
-		protected virtual async Task HandleUnknownMessageCommandAsync(Message message, BotUser user) {
-			await bot.Client.SendTextMessageAsync(message.From.Id,
-				text: Localization.GetMessage("UnknownCommand", user.Lang));
-			//await Task.FromResult(0);
-		}
-
-		protected virtual async Task HandleUnknownCallbackQueryCommandAsync(CallbackQuery callbackQuery, BotUser user) {
-			await bot.Client.AnswerCallbackQueryAsync(callbackQuery.Id,
-				text: Localization.GetMessage("UnknownCommand", user.Lang),
-				showAlert: true);
-			//await Task.FromResult(0);
-		}
-
+		protected virtual async Task HandleUnknownMessageCommandAsync(Message message, BotUser user)
+			=> await bot.Client.SendTextMessageAsync(message.From.Id,
+				Localization.GetMessage("UnknownCommand", user.Lang));
+		
+		protected virtual async Task HandleUnknownCallbackQueryCommandAsync(CallbackQuery callbackQuery, BotUser user)
+			=> await bot.Client.AnswerCallbackQueryAsync(callbackQuery.Id,
+				Localization.GetMessage("UnknownCommand", user.Lang),
+				true);
+		
 	}
+
 }
