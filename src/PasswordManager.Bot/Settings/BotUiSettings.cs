e3a73d0383e059de37789df252f166bad84f8e1c
Adam Evans <zankomag@gmail.com>
2022-05-02 15:07:07 +0300

    Move AccountSeparator to BotUiSettings

--- src/PasswordManager.Bot/Settings/BotUiSettings.cs
+++ src/PasswordManager.Bot/Settings/BotUiSettings.cs
@@ -7,6 +7,14 @@ public class BotUiSettings : ISettings {
 	
 	public string SectionName => "TelegramBotUi";
 
+	/// <summary>
+	/// Indicates how many accounts can be on a page
+	/// </summary>
 	public int PageSize { get; set; }
+	
+	/// <summary>
+	/// Indicates separating between account records on a page
+	/// </summary>
+	public string AccountSeparator { get; set; }
 
 }
