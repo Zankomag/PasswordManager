29f05eeda4e1fa08dc12bfcc88b12b4bf98f9220
Adam Evans <Zankomag@gmail.com>
2022-05-23 05:09:59 +0300

    Add config options registration to Common

---
+++ src/PasswordManager.Common/Utilities/OptionsValidator.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.Extensions.Options;
+
+namespace PasswordManager.Common.Utilities;
+
+
+//todo move to library Junetic.AspNetCore.Common
+public sealed class OptionsValidator<TOptions> : IValidateOptions<TOptions> where TOptions : class {
+
+	public ValidateOptionsResult Validate(string name, TOptions options) {
+		ValidationContext validationContext = new ValidationContext(options);
+		List<ValidationResult> validationResults = new List<ValidationResult>();
+		bool noValidationErrorsOccured = Validator.TryValidateObject(options, validationContext, validationResults, true);
+
+		if(noValidationErrorsOccured) {
+			return ValidateOptionsResult.Success;
+		}
+
+		IEnumerable<string> validationFailures = validationResults.Select(validationResult => validationResult.ErrorMessage);
+
+		return ValidateOptionsResult.Fail(validationFailures);
+	}
+
+}
